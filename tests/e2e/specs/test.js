aaa758cd1fdc4546d31f82c9fc95f516eae2978e
Nils <nils@vaede.com>
2019-12-08 22:47:32 +0100

    [test e2e] multiple contacts

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -81,5 +81,47 @@ describe('App with one contact', () => {
     cy.contains('span', 'Déconnexion').click()
     cy.url().should('eq', 'http://localhost:8080/')
   })
-  
+})
+
+describe('Multiple contacts', () => {
+  it('Login', () => {
+    cy.visit('http://localhost:8080/app')
+    cy.get('#home-choise-logIn').click()
+    cy.contains('h3', 'Connexion')
+    cy.get('input[type=email]').type(email)
+    cy.get('input[type=password]').type(password)
+    cy.get('button[type=submit]').click()
+    
+    cy.contains('button', 'Ajouter un contact')
+  })
+
+  it('Create 3 contacts', () => {
+    cy.contains('button', 'Ajouter un contact').click()
+    cy.get('input[placeholder="Prénom"]').type('Alex')
+    cy.get('#add-btn').click()
+
+    cy.contains('button', 'Ajouter un contact').click()
+    cy.get('input[placeholder="Prénom"]').first().type('Alexandre')
+    cy.get('#add-btn').click()
+
+    cy.wait(500)
+    cy.contains('button', 'Ajouter un contact').click()
+    cy.get('input[placeholder="Prénom"]').first().type('Eva')
+    cy.get('#add-btn').click()
+
+    cy.get('.contact-wrapper').should('have.length', 3)
+  })
+
+  it('Search for John', () => {
+    cy.get('input[placeholder="Rechercher un contract"]').type('John')
+    cy.get('.contact-wrapper').should('have.length', 0)
+  })
+
+  it('Search for Alex', () => {
+    cy.get('input[placeholder="Rechercher un contract"]').clear()
+    cy.get('input[placeholder="Rechercher un contract"]').type('Alex')
+    // cypress going to fast for the API
+    cy.wait(750)
+    cy.get('.contact-wrapper').should('have.length', 2)
+  })
 })
